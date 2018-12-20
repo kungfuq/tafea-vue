b6dabaaaea8510e92325ef21c12fe94f024a167c
kungfu.zsq <zsq@icloud.com>
2018-12-20 11:01:02 +0800

    前端调用服务器代码

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://demo.renren.io/renren-fast';
+  window.SITE_CONFIG['baseUrl'] = 'http://101.201.73.132:9001/renren-fast';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
